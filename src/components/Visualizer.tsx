0c3eca586ad4923595d9c6ace9e46473821e08a0
simonlary <simonlary@gmail.com>
2024-05-23 00:12:08 -0400

    Fix lint

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -1,5 +1,4 @@
 import GraphVisualizer from "./GraphVisualizer";
-import Metrics from "./Metrics";
 import { useMemo } from "react";
 
 export default function Visualizer({ distributions }: Props) {
@@ -15,7 +14,7 @@ export default function Visualizer({ distributions }: Props) {
 		<div className="flex w-full flex-wrap items-stretch justify-center gap-2">
 			{data.map((data, index) => (
 				<section key={index} className="flex w-full max-w-3xl flex-col items-center justify-between">
-					<h2 className="text-white font-bold text-xl">Output {index + 1}</h2>
+					<h2 className="text-xl font-bold text-white">Output {index + 1}</h2>
 					{/* <Metrics data={data} /> */}
 					<GraphVisualizer data={data} />
 				</section>
