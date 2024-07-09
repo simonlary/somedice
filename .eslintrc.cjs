ee6d87521c3239107e2ae8c4ec150720638373db
simonlary <simonlary@gmail.com>
2024-07-08 21:53:38 -0400

    Update dependencies.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,9 @@
 module.exports = {
 	root: true,
 	env: { browser: true, es2020: true },
-	settings: {
-		react: {
-			version: "detect",
-		},
-	},
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended-type-checked",
-		"plugin:@typescript-eslint/stylistic-type-checked",
-		"plugin:react/recommended",
-		"plugin:react/jsx-runtime",
-		"plugin:react-hooks/recommended",
-		"prettier",
-	],
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "prettier"],
 	ignorePatterns: ["dist"],
 	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
-		project: ["./tsconfig.json", "./tsconfig.node.json"],
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-		tsconfigRootDir: __dirname,
-	},
 	plugins: ["react-refresh"],
 	rules: {
 		"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
